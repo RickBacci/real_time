662cdbd6dd4b53f1ee0ebd765e18a890f0704a87
Rick Bacci <rickbacci@gmail.com>
2015-11-11 15:30:12 -0700

    add react Landing class
    - add react Scheduling button class

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -1,12 +1,38 @@
 import '../styles/base.css';
 import React from 'react/addons';
 
-let Name = React.createClass({
-  render() {
+let Landing = React.createClass({
+  getInitialState: function () {
+    return { timeSlots: [], scheduleUrl: '', appointmentsUrl: '' }
+  },
+  handleScheduleButtonClick: function() {
+    this.setState({ timeSlots: ["Rick Bacci"] });
+  },
+  render: function() {
     return(
-      <h1>{this.props.name}!</h1>
+      <div>
+        <h1>{ this.props.name }!</h1>
+        <h3>Scheduling Url:{ this.state.timeSlots }</h3>
+        <h3>Appointments Url:{ this.state.timeSlots }</h3>
+        <NewScheduleButton onButtonClick={this.handleScheduleButtonClick} />
+      </div>
     )
   }
 });
 
-React.render(<Name name='Real Time' />, document.body);
+
+let NewScheduleButton = React.createClass({
+  handleClick: function() {
+    this.props.onButtonClick();
+  },
+  render: function() {
+    return(
+      <button className='btn btn-primary btn-large' onClick={ this.handleClick } >
+        Create New Schedule
+      </button>
+    )
+  }
+});
+
+
+React.render(<Landing name='Real Time' />, document.getElementById('content'));
